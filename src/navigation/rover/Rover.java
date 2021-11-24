be27c9c93c4218b4c61eb1f296819871ce2c0f32
MarcusMedeiros99 <marcuspara66@usp.br>
2021-11-24 16:17:53 -0400

    Linking rover instance to a Direction

--- src/navigation/rover/Rover.java
+++ src/navigation/rover/Rover.java
@@ -1,15 +1,17 @@
 package navigation.rover;
+import navigation.Direction;
 import navigation.Position;
 import navigation.world.World;
 
 public class Rover {
   protected Position position;
   protected World world;
+  protected Direction direction;
 
-  public Rover(Position position, World world) {
+  public Rover(Position position, Direction direction, World world) {
     this.world = world;
     this.position = position;
-
+    this.direction = direction;
   }
 
   public Position getPosition() {
@@ -18,6 +20,10 @@ public class Rover {
 
   public World getWorld() {
     return world;
+  }
+
+  public Direction getDirection() {
+    return direction;
   }
 
 }
