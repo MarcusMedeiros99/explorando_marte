fbaf50c1a873baf0a98189928116104e4b021cb0
Marcus Medeiros <marcuspara66@usp.br>
2021-12-17 14:52:04 -0400

    Configuring pom.xml

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
+import java.net.ServerSocket;
+import java.net.http.WebSocket;
+
 import controller.InputReader;
+import controller.web.HttpServerWrapper;
 import navigation.Environment;
 import view.OutputWriter;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Environment environment = InputReader.parseFile(args[0]);
-        OutputWriter.writeOutput(environment);
+        // OutputWriter.writeOutput(environment);
+
+
     }
 }
