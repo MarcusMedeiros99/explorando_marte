c88767ff61e8f6958034025bacdd948bfbc1a27a
Marcus Medeiros <marcuspara66@usp.br>
2021-12-21 19:26:51 -0400

    fix: broadcast rover info when RoverCommandEvent.fire()

--- src/controller/web/events/RoverCommandEvent.java
+++ src/controller/web/events/RoverCommandEvent.java
@@ -4,10 +4,12 @@ import controller.commands.Command;
 import controller.commands.CommandFactory;
 import controller.constants.CommandCode;
 import controller.web.RoverEndpoint;
+import navigation.rover.Rover;
 
 public class RoverCommandEvent implements Event {
     CommandCode commandCode;
     int roverIndex;
+    Rover rover;
 
     public RoverCommandEvent() {
         
@@ -15,13 +17,14 @@ public class RoverCommandEvent implements Event {
 
     public RoverCommandEvent(CommandCode commandCode, int roverIndex) {
         this.commandCode = commandCode;
-        this.roverIndex = roverIndex;
+        this.roverIndex = roverIndex;    
     }
 
     @Override
     public void fire() {
+        this.rover = RoverEndpoint.getEnvironment().getRovers().get(roverIndex);
         Command command = new CommandFactory().createCommand(commandCode);
-        command.execute(RoverEndpoint.getEnvironment().getRovers().get(roverIndex));
+        command.execute(this.rover);
     }
 
     
